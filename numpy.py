bf2e2bac4b784164fcb555c7e8f06e3bf29d9f36
roamiriii <149173245+roamiriii@users.noreply.github.com>
2023-10-27 18:17:56 +0330

    Update numpy.py
    Add three matrix multiplication code

--- numpy.py
+++ numpy.py
@@ -1,27 +1,42 @@
 import numpy as np
 
-# ایجاد دو ماتریس تصادفی با ابعاد 3x3
+#Create two 3x3 random matrices
 matrix1 = np.random.rand(3, 3)
 matrix2 = np.random.rand(3, 3)
 
-# چاپ ماتریس‌های ایجاد شده
-print("ماتریس 1:")
+# Print the created matrices
+print("matrix 1:")
 print(matrix1)
-print("ماتریس 2:")
+print("matrix 2:")
 print(matrix2)
 
-# جمع دو ماتریس
+# Addition of two matrices
 result_sum = matrix1 + matrix2
-print("جمع دو ماتریس:")
+print("Addition of two matrices:")
 print(result_sum)
 
-# ضرب یک ماتریس در یک عدد صحیح
+# Multiplying a matrix by an integer
 scalar = 2
 result_scalar_multiply = matrix1 * scalar
-print(f"ضرب ماتریس 1 در {scalar}:")
+print(f"Matrix multiplication in {scalar}:")
 print(result_scalar_multiply)
 
-# ضرب دو ماتریس
+# Multiplication of two matrices
 result_matrix_multiply = np.dot(matrix1, matrix2)
-print("ضرب دو ماتریس:")
+print("Multiplication of two matrices:")
 print(result_matrix_multiply)
+
+# Multiplication of three matrices
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+B = np.array([[9, 8, 7],
+              [6, 5, 4],
+              [3, 2, 1]])
+
+C = np.array([[0, 1, 0],
+              [1, 0, 1],
+              [0, 1, 0]])
+result = np.dot(np.dot(A, B), C)
+print("The result of multiplying three matrices:")
